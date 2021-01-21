39335e5fa03698cb15f8cb54e47d2a1f222e48e7
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-01-20 19:03:30 -0700

    set the initial state of the store

---
+++ src/store/reducer.ts
@@ -0,0 +1,19 @@
+import * as actionTypes from './actionTypes';
+
+// sets the initial state of the store
+const initialState: ArticleState = {
+  articles: [
+    {
+      id: 1,
+      title: 'post 1',
+      body:
+        'Quisque cursus, metus vitae pharetra Nam libero tempore, cum soluta nobis est eligendi'
+    },
+    {
+      id: 2,
+      title: 'post 2',
+      body:
+        'Harum quidem rerum facilis est et expedita distinctio quas molestias excepturi sint'
+    }
+  ]
+}
