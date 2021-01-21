5156a04166076c69ab6258aae08d2888cf128f9f
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-01-20 19:11:06 -0700

    updated the reducer functions for adding and removing an article

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -17,3 +17,36 @@ const initialState: ArticleState = {
     }
   ]
 }
+
+// takes the type, performs an action to return a specific result
+const reducer = (
+  state: ArticleState = initialState,
+  action: ArticleAction
+): ArticleState => {
+  switch (action.type) {
+    case actionTypes.ADD_ARTICLE:
+      const newArticle: IArticle = {
+        id: Math.random(), // not really unique
+        title: action.article.title,
+        body: action.article.body
+      }
+      return {
+        ...state,
+        articles: state.articles.concat(newArticle),
+      }
+      
+      case actionTypes.REMOVE_ARTICLE:
+        const updatedArticles: IArticle[] = state.articles.filter(
+          article => article.id !== action.article.id
+        )
+
+      return {
+        ...state,
+        articles: updatedArticles,
+      }
+    }
+
+    return state;
+  }
+
+  export default reducer;
